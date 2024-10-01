92a1eadec15b6e06a9bbe2df384b505c5239011a
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-01 17:08:40 +0200

    final-adjustments

--- script.js
+++ script.js
@@ -6,28 +6,34 @@
 //Scrivi un programma che stampi i numeri da 1 a 100---> deduco che la condizione sarà i<=100 e l'incremento sarè ++i
 //Invece l'inizializzazione sarà i= 1
 
-//ma per i multipli di 3 stampi “Fizz” al posto del numero ---> deduco che dovrò sicuramente usare un If e un Else e che quando i sarà moltiplicata x 3 il CONSOLE LOG dovrà essere "Fizz"
-//e per i multipli di 5 stampi "Buzz" ----> deduco la stessa cosa ma i x 5 e invece di riscrivere If ed Else aggiungerò un Else if
-//Per i numeri che sono siano multipli di 3 che di 5 stampi "FizzBuzz" ----> deduco che dovrò aggiungere un altro Else if
+//ma per i multipli di 3 stampi “Fizz” al posto del numero ---> deduco che dovrò sicuramente usare un If e un Else e che quando i sarà diviso da 3 e darà resto 0 (i % 3 == 0) il CONSOLE LOG dovrà essere "Fizz"
+//e per i multipli di 5 stampi "Buzz" ----> deduco la stessa cosa ma i % 5 ==0 e invece di riscrivere If ed Else aggiungerò un Else if
+//Per i numeri che sono siano multipli di 3 che di 5 stampi "FizzBuzz" ----> deduco che dovrò aggiungere un altro Else if con &&
+// Invece L'else riguarderà i numeri che non rientrano in nessuna delle 3 categorie qua sopra.
 
 
 
 
 for (i = 1; i <= 100; ++i) {
 
-    if (i = i * 3) {
+  
+    if (i % 5 == 0 && i % 3 == 0)  {
+        console.log('Fizzbuzz');
+
+    }
+
+    else if (i % 3 == 0) {
         console.log('Fizz');
 
     }
 
-    else if (i = i * 5) {
+    else if (i % 5 == 0) {
         console.log('Buzz');
 
     }
 
     else {
-        console.log('Fizzbuzz');
-
+        console.log(i);
     }
 
 
