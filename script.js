403de102fe1e46f37e9140ac1e83c56d3e8e3a8a
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-01 15:14:01 +0200

    comments-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Consegna:
+//Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+//Prima di partire a scrivere codice poniamoci qualche domanda: Come faccio a sapere se un numero è divisibile per? Abbiamo visto qualcosa di particolare che possiamo usare?
+
+
+//Scrivi un programma che stampi i numeri da 1 a 100---> deduco che la condizione sarà i<=100 e l'incremento sarè ++i
+//Invece l'inizializzazione sarà i= 1
+
+//ma per i multipli di 3 stampi “Fizz” al posto del numero ---> deduco che dovrò sicuramente usare un If e un Else e che quando i sarà moltiplicata x 3 il CONSOLE LOG dovrà essere "Fizz"
+//e per i multipli di 5 stampi "Buzz" ----> deduco la stessa cosa ma i x 5 e invece di riscrivere If ed Else aggiungerò un Else if
+//Per i numeri che sono siano multipli di 3 che di 5 stampi "FizzBuzz" ----> deduco che dovrò aggiungere un altro Else if
+
